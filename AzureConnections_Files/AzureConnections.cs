f841d40ca1d6fa20fd3c8cb8d0a11664c853ca5d
David Jones <davidjones@sportronics.com.au>
2019-05-30 12:18:04 +1000

    Some changes to use shared source file between projects such as AzureConnections for DNC and UWP

---
+++ AzureConnections_Files/AzureConnections.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureConnections
+{
+    public static class MyConnections
+    {
+        
+        public static string IoTHubConnectionString = "";
+        public static string DeviceId = "";
+        public static string DeviceConnectionString = "";
+    }
+}
